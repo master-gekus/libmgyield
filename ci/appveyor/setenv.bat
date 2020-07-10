571721e199002829120633af1e22cd8a475abe11
Evgeny V. Zalivochkin <master.gekus@gmail.com>
2020-07-10 14:40:46 +0300

    Try to check

--- ci/appveyor/setenv.bat
+++ ci/appveyor/setenv.bat
@@ -3,6 +3,23 @@ set _vs=%1
 set _ps=%2
 set _vs=%_vs:"=%
 set _ps=%_ps:"=%
-echo "%_vs%: %_ps%"
-call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+echo "%_vs%:  %_ps%"
+if /i "%_vs%" == "Visual Studio 2015" (
+  if /i "%_ps%" == "x64" (
+    call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+    goto done
+  )
+  if /i "%_ps%" == "x86" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+    goto done
+  )
+  echo Unsupported platform: %_ps%
+  goto error
+)
+
+echo Version of Visual studio is not supported: %_vs%
+:error
+exit 1
+
+:done
