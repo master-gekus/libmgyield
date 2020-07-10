b3f0f811920b7e0b0026739f143ef116c05c5a08
Evgeny V. Zalivochkin <master.gekus@gmail.com>
2020-07-10 14:38:53 +0300

    Variables

--- ci/appveyor/setenv.bat
+++ ci/appveyor/setenv.bat
@@ -1,4 +1,8 @@
 @echo off
-echo %1 %2
+set _vs=%1
+set _ps=%2
+set _vs=%_vs:"=%
+set _ps=%_ps:"=%
+echo "%_vs%: %_ps%"
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
