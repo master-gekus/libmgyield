08e1beecf8cd7fe92d5602c998adaafdf7cf90ec
Evgeny V. Zalivochkin <master.gekus@gmail.com>
2020-07-10 14:35:21 +0300

    No error in .BAT file

--- ci/appveyor/setenv.bat
+++ ci/appveyor/setenv.bat
@@ -2,5 +2,3 @@
 echo %1 %2
 call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
-echo "This platform is not supported!"
-exit 1
